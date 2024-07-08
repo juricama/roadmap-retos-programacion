dcad570f43cd950a4aaee3214b80d4c27d1d80f5
juricama <echerjon@gmail.com>
2024-07-07 23:27:32 -0500

    Create juricama.py
    primer reto de programación en Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/juricama.py
@@ -0,0 +1,70 @@
+# gracias Brais por compartir y comenzamos con el reto
+
+# https://www.python.org/
+
+# Un comentario comienza con un carácter de almohadilla (#)
+
+"""
+Para hacer comentarios
+en varias lineas de codigo
+se utiliza tres dobles comillas al comienzo
+y tres dobles comillas al final
+"""
+
+'''
+Tambien podemos hacer comentarios
+en varias lineas de codigo
+utilizando tres comillas sencillas al comienzo
+y tres comillas sencillas al final
+'''
+
+# En Python una variable es un contenedor capaz de almacenar información como texto, números, valores lógicos, etc.
+'''
+Para inicializar una variable
+(darle un valor inicial cuando la creamos),
+hay que utilizar el operador de asignación («=»)
+'''
+mensaje = 'mensaje en una variable.'
+
+"""
+Las constantes de Python
+son contenedores de datos como las variables.
+Sin embargo, en Python no existen las constantes como tal.
+"""
+# La convención para nombrar constantes es hacerlo con el nombre todo en mayúsculas
+HORAS_DIA = 24
+DIAS_SEMANA = 7
+
+# Datos primitivos en Python son 4
+
+'''
+numero 1
+es el entero o integer
+'''
+my_int = 4
+
+'''
+numero 2
+es el decimal o float
+'''
+my_float = 3.8
+
+'''
+numero 3
+es el buleano o bulean
+que puede ser verdadero (True)
+o que puede ser falso (False)
+'''
+my_bool = True
+
+'''
+numero 4
+es una cadena de texto
+ostring
+'''
+my_str = "Una cadena de texto"
+
+# para terminar el reto imprimr "¡Hola, [y el nombre de tu lenguaje]!"
+# don de utilizamos print
+
+print("¡Hola, Python!")
